a324157a235431513dafebb2c6089cfd4e439259
smtwtfs400800 <114556345+smtwtfs400800@users.noreply.github.com>
2022-12-17 20:44:00 +0900

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import obfuscation
-import deobfuscation
-
-# 주석주석주석주석
-if __name__ == '__main__':
-    user_name = obfuscation.encrypt('soft_sec')
-    user_id = obfuscation.encrypt('12345678')
-    print('user_name : ' + user_name)
-    print('user_id : ' + user_id)
